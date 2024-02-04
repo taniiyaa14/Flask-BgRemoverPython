7364a4c822ec456108deafe56d46d1be6ec644a7
taniiyaa14 <157497630+taniiyaa14@users.noreply.github.com>
2024-02-04 21:03:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,37 @@
-from flask import Flask,render_template,request,redirect,url_for
+from flask import Flask, render_template, request
 from rembg import remove
 from PIL import Image
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
-@app.route('/',methods=['GET','POST'])
+def process_image(input):
+    inp = Image.open(input)
+    inpp = remove(inp)
+    image = inpp.convert("RGBA")
+
+    background_color = (255, 255, 255)
+    tolerance = 30
+
+    new_data = [
+        (255, 255, 255, 0) if all(abs(c - background_color[i]) <= tolerance for i, c in enumerate(item[:3])) else item
+        for item in image.getdata()
+    ]
+
+    image.putdata(new_data)
+
+    return image
+
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    if request.method=="POST":
-        input=request.files['imageUpload']
-        inp=Image.open(input)
-        inpp=remove(inp)
-        image=inpp.convert("RGBA")
-        data = image.getdata()
-        new_data = []
-        background_color = (255, 255, 255)
-        tolerance = 30
-        for item in data:
-            if all(abs(c - background_color[i]) <= tolerance for i, c in enumerate(item[:3])):
-                new_data.append((255, 255, 255, 0)) 
-            else:
-                new_data.append(item)
+    if request.method == "POST":
+        input_file = request.files['imageUpload']
+        processed_image = process_image(input_file)
 
-        image.putdata(new_data)
+        # Save processed image
+        processed_image.save("static/images/img1.png")
 
-        image.save("static/images/img1.png")
-
-        filename=secure_filename(input.filename)
-        return render_template("index.html",filename=filename)
+        filename = secure_filename(input_file.filename)
+        return render_template("index.html", filename=filename)
+    
     return render_template("index.html")
-
-
